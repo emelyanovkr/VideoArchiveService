24eae853c5a0c36eaddcdd6457f17131fa07882e
Kirill Emelyanov <k.emelyanov070@gmail.com>
2024-09-10 19:08:32 +0300

    init repo

---
+++ src/main/java/helium/video/VideoArchiveService/VideoArchiveServiceApplication.java
@@ -0,0 +1,13 @@
+package helium.video.VideoArchiveService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoArchiveServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoArchiveServiceApplication.class, args);
+	}
+
+}
