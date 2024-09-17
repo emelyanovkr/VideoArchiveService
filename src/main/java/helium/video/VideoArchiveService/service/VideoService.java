32d4e2de61d2cb4b37e486201947b29e418cd070
Kirill Emelyanov <k.emelyanov070@gmail.com>
2024-09-17 16:36:48 +0300

    uploading big files over rest, make writing method asynchronous

--- src/main/java/helium/video/VideoArchiveService/service/VideoService.java
+++ src/main/java/helium/video/VideoArchiveService/service/VideoService.java
@@ -2,22 +2,23 @@ package helium.video.VideoArchiveService.service;
 
 import helium.video.VideoArchiveService.model.VideoEntry;
 import helium.video.VideoArchiveService.repository.VideoRepository;
+import helium.video.VideoArchiveService.util.MessageConstants;
+import jakarta.servlet.http.HttpServletRequest;
 import org.apache.tika.Tika;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.metadata.TIFF;
 import org.apache.tika.metadata.XMPDM;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.lang.Nullable;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class VideoService {
@@ -38,45 +39,44 @@ public class VideoService {
     this.metadata = new Metadata();
   }
 
-  public void saveVideoFile(MultipartFile file) throws IOException, RuntimeException {
-    if (file == null || file.isEmpty()) {
-      // TODO: create CUSTOM EXCEPTION
-      throw new RuntimeException("File is null or empty");
-    }
+  @Async
+  public CompletableFuture<String> saveVideoFile(HttpServletRequest request, String fileName)
+      throws IOException {
 
     if (DATA_STORAGE_PATH == null) {
       throw new RuntimeException();
     }
 
-    File localSavedFile = writeVideoFileToFileSystem(file);
-
-    tika.parse(localSavedFile, metadata);
-    System.out.println(metadata.get(XMPDM.DURATION));
-
-    VideoEntry videoEntry =
-        new VideoEntry(
-            localSavedFile.getName(),
-            LocalDateTime.now(), // TODO: TEMPORARY
-            Double.parseDouble(metadata.get(XMPDM.DURATION)),
-            (double) localSavedFile.length() / (1024 * 1024),
-            metadata.get(TIFF.IMAGE_WIDTH) + "x" + metadata.get(TIFF.IMAGE_LENGTH));
-
-    videoRepository.save(videoEntry);
-  }
-
-  private File writeVideoFileToFileSystem(MultipartFile file) throws IOException {
     Path uploadDirPath = Path.of(DATA_STORAGE_PATH);
     if (!Files.exists(uploadDirPath)) {
       Files.createDirectory(uploadDirPath);
     }
 
-    Path filePath = uploadDirPath.resolve(file.getOriginalFilename());
-    File localSavedFile = filePath.toFile();
-
-    try (OutputStream outputStream = new FileOutputStream(localSavedFile)) {
-      outputStream.write(file.getBytes());
+    Path filePath = uploadDirPath.resolve(fileName);
+    if (Files.exists(filePath)) {
+      return CompletableFuture.completedFuture(MessageConstants.FILE_ALREADY_EXIST_MSG);
     }
 
-    return localSavedFile;
+    try (InputStream inputStream = request.getInputStream()) {
+      Path destination = Path.of(DATA_STORAGE_PATH, fileName);
+
+      Files.copy(inputStream, destination, StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    File localSavedFile = filePath.toFile();
+
+    try (Reader ignored = tika.parse(localSavedFile, metadata)) {
+      VideoEntry videoEntry =
+          new VideoEntry(
+              localSavedFile.getName(),
+              LocalDateTime.now(), // TODO: TEMPORARY
+              Double.parseDouble(metadata.get(XMPDM.DURATION)),
+              (double) localSavedFile.length() / (1024 * 1024),
+              metadata.get(TIFF.IMAGE_WIDTH) + "x" + metadata.get(TIFF.IMAGE_LENGTH));
+
+      videoRepository.save(videoEntry);
+    }
+
+    return CompletableFuture.completedFuture(MessageConstants.FILE_SAVED_SUCCESS_MSG);
   }
 }
---
+++ src/main/java/helium/video/VideoArchiveService/util/MessageConstants.java
@@ -0,0 +1,6 @@
+package helium.video.VideoArchiveService.util;
+
+public class MessageConstants {
+  public static final String FILE_ALREADY_EXIST_MSG = "File already exist";
+  public static final String FILE_SAVED_SUCCESS_MSG = "File saved successfully";
+}
